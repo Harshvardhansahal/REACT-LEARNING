ec18dda4bc2d1c88c8f1d31ee54789b0281b2b33
HarshvardhanSahal <harshvardhansahal@gmail.com>
2023-12-27 22:43:10 +0530

    Using Props

--- Fragments/Learning-Fragments/src/App.jsx
+++ Fragments/Learning-Fragments/src/App.jsx
@@ -1,19 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import FoodItems from "./Components/FoodItems";
+import ErrorMessage from "./Components/ErrorMessage";
 function App() {
   // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", , "Milk"];
-  let foodItems = [];
+  // let foodItems = [];
 
   return (
     <>
       <h1>Healthy Food</h1>
-      {foodItems.length === 0 && <h3>I am Still hungry</h3>}
-      <ul className="list-group">
-        {foodItems.map((item) => (
-          <li key={item} className="list-group-item">
-            {item}
-          </li>
-        ))}
-      </ul>
+      <ErrorMessage></ErrorMessage>
+      <FoodItems></FoodItems>
     </>
   );
 }
---
+++ Fragments/Learning-Fragments/src/Components/Items.jsx
@@ -0,0 +1,4 @@
+const Items = (props) => {
+  return <li className="list-group-item">{props.foodItem}</li>;
+};
+export default Items;
