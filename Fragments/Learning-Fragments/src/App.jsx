c97fe59dd0eaa50e7ec70b0bcd8ac40edd0e6a78
HarshvardhanSahal <harshvardhansahal@gmail.com>
2023-12-27 22:13:53 +0530

    using if Statement Part of conditional rendering

--- Fragments/Learning-Fragments/src/App.jsx
+++ Fragments/Learning-Fragments/src/App.jsx
@@ -1,6 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", , "Milk"];
+  // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", , "Milk"];
+  let foodItems = [];
+  if (foodItems.length === 0) {
+    return <h3>I am Still hungry</h3>;
+  }
   return (
     <>
       <h1>Healthy Food</h1>
