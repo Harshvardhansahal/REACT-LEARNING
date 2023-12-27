3228e393c63510d440e9681c0389724b070a0aef
HarshvardhanSahal <harshvardhansahal@gmail.com>
2023-12-27 22:01:47 +0530

    Using Map Method

--- Fragments/Learning-Fragments/src/App.jsx
+++ Fragments/Learning-Fragments/src/App.jsx
@@ -1,14 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", , "Milk"];
   return (
     <>
       <h1>Healthy Food</h1>
       <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+        {foodItems.map((item) => (
+          <li class="list-group-item">{item}</li>
+        ))}
       </ul>
     </>
   );
