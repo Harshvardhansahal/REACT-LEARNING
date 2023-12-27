25775e2c40a7ef8ca807101e0104c6826d4d6c5a
HarshvardhanSahal <harshvardhansahal@gmail.com>
2023-12-27 22:17:09 +0530

    using Ternary Operators Part of Conditional Rendering

--- Fragments/Learning-Fragments/src/App.jsx
+++ Fragments/Learning-Fragments/src/App.jsx
@@ -2,12 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   // let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", , "Milk"];
   let foodItems = [];
-  if (foodItems.length === 0) {
-    return <h3>I am Still hungry</h3>;
-  }
+
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 ? <h3>I am Still hungry</h3> : null}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
