173ea48f854b7cf85451d211fb390f8141dc24d9
HarshvardhanSahal <harshvardhansahal@gmail.com>
2023-12-27 22:19:34 +0530

    using Logical Operator Part of Conditional Rendering

--- Fragments/Learning-Fragments/src/App.jsx
+++ Fragments/Learning-Fragments/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1>Healthy Food</h1>
-      {foodItems.length === 0 ? <h3>I am Still hungry</h3> : null}
+      {foodItems.length === 0 && <h3>I am Still hungry</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
