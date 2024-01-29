4720a15fee234874ce09f240537d4c8d645df99a
HarshvardhanSahal <harshvardhansahal@gmail.com>
2024-01-29 22:18:55 +0530

    calculator all keys functionality working

---
+++ Calculator-version2/src/Components/Display.jsx
@@ -0,0 +1,14 @@
+import styles from "../Components/Display.module.css";
+
+const Display = ({ displayValue }) => {
+  return (
+    <input
+      className={styles.display}
+      type="text"
+      value={displayValue}
+      readOnly
+    />
+  );
+};
+
+export default Display;
